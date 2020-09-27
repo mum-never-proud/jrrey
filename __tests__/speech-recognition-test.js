a469c43f0cebe4164744b35d3b536e4780a29588
mum-never-proud <abhuz@hotmail.com>
2020-09-27 14:38:44 +0530

    tests fix

--- __tests__/speech-recognition-test.js
+++ __tests__/speech-recognition-test.js
@@ -1,11 +1,25 @@
+/* eslint-disable no-console */
 /* eslint-disable global-require */
 describe('speech recognition', () => {
+  const nativeConsoleWarn = console.warn;
+
   beforeEach(() => {
     jest.resetModules();
   });
 
+  beforeAll(() => {
+    console.warn = jest.fn();
+  });
+
+  afterAll(() => {
+    console.warn = nativeConsoleWarn;
+  });
+
   it('should throw error when speech recognition is not supported', () => {
-    expect(() => { require('../src/utils/speech-recognition'); }).toThrowError(Error);
+    const speechRecognition = require('../src/utils/speech-recognition').default;
+
+    expect(console.warn).toHaveBeenCalled();
+    expect(speechRecognition).toBeNull();
   });
 
   it('should return speech recognition object', () => {
